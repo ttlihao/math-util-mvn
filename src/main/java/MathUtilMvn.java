9473bc204b626992277dbcefd2ac326b2771810c
ttlihao <thienphan20122003@gmail.com>
2023-10-07 17:09:51 +0700

    1st upload

---
+++ src/main/java/MathUtilMvn.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Phan Thien
+ */
+public class MathUtilMvn {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
